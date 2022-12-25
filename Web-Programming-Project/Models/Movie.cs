81bcb3095582cf88bfd875e21f79667cb68f2131
Mazhar <mazhar.akyoldas@gmail.com>
2022-12-25 03:59:47 +0300

    Some changes

---
+++ Web-Programming-Project/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿namespace Web_Programming_Project.Models
+{
+    public class Movie
+    {
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Director { get; set; }
+
+        public string Composer { get; set; }
+
+        public string Language { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Writer { get; set; }
+
+        public string Star_Actor { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Synposis { get; set; }
+
+
+    }
+}
